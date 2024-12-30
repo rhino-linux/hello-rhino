9fa79980a095eaa0c58c349d04e5bd60d7467f75
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-12-30 15:26:48 -0500

    fix: `launch_on_start` declare and autostart name (#4)
    * attempt to fix
    
    * ah
    
    * try this

--- src/main.rs
+++ src/main.rs
@@ -90,17 +90,15 @@ impl HelloRhino {
         // setup config for autostart
         let exe = std::env::current_exe().unwrap();
 
-        let auto_launch = AutoLaunch::new("Hello Rhino", exe.to_str().unwrap(), &["--minimized"]);
+        let auto_launch = AutoLaunch::new("hello-rhino", exe.to_str().unwrap(), &["--minimized"]);
 
-        // initially set the autostart to true
+        // do not use auto_launch function in debug mode
         #[allow(unused_assignments)]
-        let mut launch_on_start = true;
-        // Disable autostarting when debug running
-        if cfg!(debug_assertions) {
-            launch_on_start = auto_launch.disable().is_ok();
+        let launch_on_start = if cfg!(debug_assertions) {
+            auto_launch.disable().is_ok()
         } else {
-            launch_on_start = auto_launch.enable().is_ok();
-        }
+            auto_launch.is_enabled().unwrap_or(false)
+        };
 
         (
             Self {
